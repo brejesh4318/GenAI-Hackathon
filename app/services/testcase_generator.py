22b5eae8d2398309cded5b720b11015932b5dca4
Dhayananthan <dhaya427@gmail.com>
2025-10-25 09:01:47 +0530

    fix: increased the recursion limit

--- app/services/testcase_generator.py
+++ app/services/testcase_generator.py
@@ -2,9 +2,11 @@ from app.services.llm_services.graph_pipeline import GraphPipe
 from app.services.llm_services.llm_interface import LLMInterface
 from app.services.reponse_format import FinalOutput
 from langgraph.graph.state import CompiledStateGraph
+from langchain_core.runnables import RunnableConfig
 from app.utilities import dc_logger
 from app.utilities.singletons_factory import DcSingleton
 from app.services.llm_services.graph_state import PipelineState
+
 
 
 logger = dc_logger.LoggerAdap(dc_logger.get_logger(__name__), {"dash-test": "V1"})
@@ -15,7 +17,8 @@ class TestCaseGenerator(metaclass = DcSingleton):
         self.graph_pipe = graph_pipe
     
     def generate_testcase(self, document_path:str):
-        test_cases = self.graph_pipe.invoke_graph(document_path)
+        config = RunnableConfig(recursion_limit=50)
+        test_cases = self.graph_pipe.invoke_graph(document_path, config=config)
         return test_cases["test_cases_final"]["test_cases"]
 
 
